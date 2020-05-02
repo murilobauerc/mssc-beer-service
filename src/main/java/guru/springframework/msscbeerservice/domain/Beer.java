fde1a30468c54d78f4ddf5c02f7123309af82166
murilobauerc <panchobauer@gmail.com>
2020-05-02 13:04:13 -0300

    Hibernate Erorrs on Startup with MySQL closes #22

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -23,7 +23,6 @@ public class Beer {
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
-
     @Version
     private Long version;
 
