b8f11770948c105c6dfe7d1c555add9f6d27826c
murilo_r <murilo_r@dell.com>
2020-05-20 16:08:28 -0300

    fixing the jackson issue with @Transactional closes #28

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,17 +1,16 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class BeerEvent implements Serializable {
     private static final long serialVersionUID = -4079334062332764833L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 }
