b1742be42ee085f2191bd24932d7bd9046ee6f31
murilo_r <murilo_r@dell.com>
2020-05-20 09:27:20 -0300

    Create Beer Event Objects closes #26

---
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+    private static final long serialVersionUID = -4079334062332764833L;
+
+    private final BeerDto beerDto;
+}
